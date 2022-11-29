a2b862a57cf121f6dfc59bb470005a26a10c64af
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-29 18:56:53 -0300

    O uso do call e apply nos objetos literal

---
+++ src/callEapply.js
@@ -0,0 +1,80 @@
+/* Durante as aulas usamos bind() para ligar (bind) o valor de this a um determinado contexto. Existem outros dois métodos para manipular o contexto de this: */
+
+import chalk from "chalk"
+
+/* O método call() executa a função passando valores e parâmetros específicos para serem usados como contexto do this. Ou seja, é possível atribuir um this diferente do contexto atual ao executar a função.
+
+Um exemplo de uso de call() para especificar o contexto de this: */
+
+function exibeInfos() {
+    console.log(chalk.blue(this.nome, this.email))
+   }
+   
+   /* const user = {
+    nome: 'Mariana',
+    email: 'm@m.com'
+   } */
+   
+//    exibeInfos.call(user)
+
+/* Fazendo com que a função seja executada em determinado contexto, mesmo após ser instanciada: */
+
+function User(nome, email) {
+    this.nome = nome
+    this.email = email
+   
+    this.exibeInfos = function(){
+      console.log(chalk.green(this.nome, this.email))
+    }
+   }
+   
+   const newUser = new User('mariana', 'm@m.com')
+
+   const outroUser = {
+    nome: 'Rodrigo',
+    email: 'r@r.com'
+   }
+   
+   newUser.exibeInfos() //mariana m@m.com
+
+   newUser.exibeInfos.call(outroUser) //Rodrigo r@r.com
+
+   /* Também é possível passar parâmetros para call(), como no exemplo a seguir.
+
+Temos uma função que monta uma determinada mensagem a partir dos parâmetros nome e email. Se quiséssemos vincular os dados da mensagem a um objeto com dados de usuários, podemos usar call() passando como primeiro parâmetro o contexto a ser considerado como this (no caso, objeto user) e a partir do segundo parâmetro definimos quais os argumentos. */
+
+
+function exibeMensagem(nome, email) {
+    console.log(chalk.blue(`usuário: ${nome}, email ${email}`))
+   }
+/*     const user = {
+    nome: 'Mariana',
+    email: 'm@m.com',
+    executaFuncao: function(fn) {
+      fn.call(user, this.nome, this.email)
+    }
+   } */
+   
+   user.executaFuncao(exibeMensagem) //usuário: Mariana, email m@m.com
+/* 
+Nesse caso, a função que será executada também está sendo passada como parâmetro de executaFuncao e usamos call() para “chamar” a função com um contexto (this) específico e também argumentos específicos.
+
+apply()
+O método apply() funciona de forma semelhante ao call(), porém recebe a lista de argumentos em um array:
+*/
+
+function exibeMensagem(nome, email) {
+    console.log(`usuário: ${nome}, email ${email}`)
+   }
+    const user = {
+    nome: 'Mariana',
+    email: 'm@m.com',
+    executaFuncao: function(fn) {
+      fn.apply(user, [this.nome, this.email])
+    }
+   }
+    user.executaFuncao(exibeMensagem) //usuário: Mariana, email m@m.com
+
+    /* Usando arrays, é possível passar os argumentos via variável ou até mesmo usando a propriedade arguments que existe internamente em todo objeto. */
+
+    
--- src/objetoLiteral.js
+++ src/objetoLiteral.js
@@ -9,13 +9,28 @@ const user = {
     role: 'admin',
     ativo: true,
     exibirInfos: function(){
-        console.log(chalk.yellow(`Meu nome é ${this.nome}, e meu email  ${this.email}`))
+        console.log(chalk.yellow(`Meu nome é ${this.nome}, e meu email ${this.email} nascido em ${this.nascimento}`))
     },
 }
 
-user.nome = 'Lucca'
-user.exibirInfos()
-user.tamanho = '1,76'
-console.log(chalk.green(`meu nome é ${user.nome} e meu tamanho ${user.tamanho}`))
-
+// user.nome = 'Lucca'
+// user.exibirInfos()
+// user.tamanho = '1,76'
+// console.log(chalk.green(`meu nome é ${user.nome} e meu tamanho ${user.tamanho}`))
 // Herança de protótipo
+
+
+// tratando a palavra this que refere a isso dentro do contexto do objeto, fora do seu objeto fica sem referência 
+const exibir = function(){
+    console.log(chalk.yellow(`${this.nome} ${this.email}`))
+}
+
+const exibirNome = exibir.bind(user) // usando o bind para ligar ou prender a nova const ao objeto, mais lembrando que ao declarar a variavel ela é uma nova variavel 
+
+exibir.call(user)// usando o call para passar a função o obj de referencia
+
+
+// exibirNome()
+// exibir()
+
+// console.log(typeof exibirNome)// função 
