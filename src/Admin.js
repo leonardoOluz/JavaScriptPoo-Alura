6e01bf5ff5d45348f6832af6390ded1d71102e43
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-01 16:05:11 -0300

    Criando a função criarCurso na subclasse Admin

--- src/Admin.js
+++ src/Admin.js
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import User from "./User.js";
 
 // Admin é uma subClasse de User => Admin é child e User é Parents 
@@ -6,9 +7,12 @@ class Admin extends User {
     constructor(nome, email, nascimento, role ='admin', ativo = true){
         super(nome, email, nascimento,role,ativo)
     }
+    criarCurso(nomeDoCurso, vagas){
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
+    }
+
 }
 
-const novoAdmin = new Admin('Lucca', 'lucca@email.com','2021-01-01')
+const novoAdmin = new Admin('Lucca', 'lucca@email.com','2021-01-01')// instanciando do new Admin
 
-console.log(novoAdmin)
-console.log(novoAdmin.exibirInfos())// usando a herança de classe 
+console.log(chalk.bgBlue(novoAdmin.criarCurso('JavaScript POO', '20')))
--- src/User.js
+++ src/User.js
@@ -1,14 +1,14 @@
 // Criando uma classe com nome User
 export default class User {
-    constructor(nome, email, nascimento, role, ativo = true){
-        this.nome =  nome
+    constructor(nome, email, nascimento, role, ativo = true) {
+        this.nome = nome
         this.email = email
         this.nascimento = nascimento
         this.role = role || 'estudante'
         this.ativo = ativo
     }
-
-    exibirInfos(){
+    // Não usamos a palavra function ao declarar um metodo na classes e só passamos o nome e criamos o metodo
+    exibirInfos() {
         return `${this.nome}, ${this.email}`
     }
 }
